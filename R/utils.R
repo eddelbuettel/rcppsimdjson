dac90d84c35f6af2ead294126e7990830fc02a0d
Brendan Knapp <brendan.g.knapp@gmail.com>
2020-08-01 15:47:46 -0700

    queries and compressed files passing

--- R/utils.R
+++ R/utils.R
@@ -5,3 +5,63 @@
 .is_scalar_chr <- function(x, na_ok = FALSE) {
     length(x) == 1L && is.character(x) && (na_ok || !is.na(x))
 }
+
+.prep_input <- function(input,
+                        temp_dir,
+                        compressed_download,
+                        verbose) {
+    input[!is.na(input)] <- path.expand(input[!is.na(input)])
+    diagnosis <- .diagnose_input(input)
+
+    if (length(missing_files <- diagnosis$input[!is.na(diagnosis$input) &
+                                                !diagnosis$is_from_url &
+                                                !file.exists(diagnosis$input)])) {
+        stop("The following non-URL files don't exist:",
+             sprintf("\n\t- %s", missing_files))
+    }
+
+    if (any(diagnosis$is_from_url)) {
+        temp_files <- tempfile(
+            pattern = basename(diagnosis$input[diagnosis$is_from_url]),
+            tmpdir = temp_dir,
+            fileext = if (compressed_download) sprintf("%s.gz", diagnosis$file_ext[diagnosis$is_from_url]) else diagnosis$file_ext[diagnosis$is_from_url]
+        )
+
+        if (any(diagnosis$is_local_file_url)) {
+            download.file(
+                url = diagnosis$input[diagnosis$is_local_file_url],
+                method = "internal",
+                destfile = temp_files[diagnosis$is_local_file_url],
+                quiet = !verbose,
+            )
+        }
+
+        if (any(diagnosis$is_from_url & !diagnosis$is_local_file)) {
+            if ((.method <- getOption("download.file.method", default = "auto")) == "libcurl") {
+                download.file(
+                    url = diagnosis$input[diagnosis$is_from_url & !diagnosis$is_local_file],
+                    method = .method,
+                    destfile = temp_files[diagnosis$is_from_url & !diagnosis$is_local_file],
+                    quiet = !verbose,
+                    headers = if (compressed_download) c(`Accept-Encoding` = "gzip") else NULL
+                )
+            } else {
+                for (i in seq_along(diagnosis$input[diagnosis$is_from_url & !diagnosis$is_local_file])) {
+                    download.file(
+                        url = diagnosis$input[diagnosis$is_from_url & !diagnosis$is_local_file][[i]],
+                        destfile = temp_files[diagnosis$input[diagnosis$is_from_url & !diagnosis$is_local_file][[i]]],
+                        method = .method,
+                        quiet = !verbose,
+                        headers = if (compressed_download) c(`Accept-Encoding` = "gzip") else NULL
+                    )
+                }
+            }
+        }
+
+        diagnosis$input[diagnosis$is_from_url] <- temp_files
+    }
+
+    diagnosis
+}
+
+
--- inst/tinytest/test_issues.R
+++ inst/tinytest/test_issues.R
@@ -1,5 +1,7 @@
 if (RcppSimdJson:::.unsupportedArchitecture()) exit_file("Unsupported chipset")
 
+my_temp_dir <- paste0(tempdir(), "/rcppsimdjson-temp")
+
 # 33: prevent deletion of local files ==========================================
 # https://github.com/eddelbuettel/rcppsimdjson/issues/33
 temp_file1 <- tempfile(".json")
@@ -12,10 +14,10 @@ expect_true(all(file.exists(c(temp_file1, temp_file2))))
 fload(c(temp_file1, temp_file2), keep_temp_files = FALSE)
 expect_true(all(file.exists(c(temp_file1, temp_file2))))
 
-my_temp_dir <- paste0(tempdir(), "/rcppsimdjson-temp")
-dir.create(my_temp_dir)
-json_url <- "https://raw.githubusercontent.com/eddelbuettel/rcppsimdjson/master/inst/jsonexamples/small/demo.json"
 
+# json_url <- "https://raw.githubusercontent.com/eddelbuettel/rcppsimdjson/master/inst/jsonexamples/small/demo.json"
+json_url <- paste0("file:///", system.file("jsonexamples/small/demo.json", package = "RcppSimdJson"))
+dir.create(my_temp_dir)
 fload(json_url, temp_dir = my_temp_dir, keep_temp_files = TRUE)
 expect_true(
     length(dir(my_temp_dir)) == 1L
@@ -44,3 +46,5 @@ expect_error(
     fparse(list('[true,false]', charToRaw('[false,true]')))
 )
 
+
+unlink(my_temp_dir)
