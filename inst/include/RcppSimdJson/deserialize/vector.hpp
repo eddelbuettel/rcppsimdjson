594dcb4b04c330c8ab7a95ec6ba9ffe4d0ad55e8
Nicolas Boyer <nicolasjiaxin@gmail.com>
2021-08-05 13:08:41 -0400

    Typos in vector.

--- inst/include/RcppSimdJson/deserialize/vector.hpp
+++ inst/include/RcppSimdJson/deserialize/vector.hpp
@@ -10,7 +10,7 @@ namespace vector {
 
 template <int RTYPE, typename in_T, rcpp_T R_Type, bool has_nulls>
 inline Rcpp::Vector<RTYPE> build_vector_typed(simdjson::ondemand::array array) {
-    Rcpp::Vector<RTYPE> out(static_cast<R_xlen_t>(array,count_elements()));
+    Rcpp::Vector<RTYPE> out(static_cast<R_xlen_t>(array.count_elements()));
     R_xlen_t            i(0L);
     for (auto element : array) {
         out[i++] = get_scalar<in_T, R_Type, has_nulls>(element);
@@ -21,7 +21,7 @@ inline Rcpp::Vector<RTYPE> build_vector_typed(simdjson::ondemand::array array) {
 
 template <bool has_nulls>
 inline Rcpp::Vector<REALSXP> build_vector_integer64_typed(simdjson::ondemand::array array) {
-    std::vector<int64_t> stl_vec_int64(static_cast<R_xlen_t>(array,count_elements()));
+    std::vector<int64_t> stl_vec_int64(static_cast<R_xlen_t>(array.count_elements()));
     std::size_t          i(0ULL);
     for (auto element : array) {
         stl_vec_int64[i++] = get_scalar<int64_t, rcpp_T::i64, has_nulls>(element);
@@ -75,14 +75,14 @@ inline SEXP dispatch_typed(simdjson::ondemand::array array, const rcpp_T R_Type,
                              : build_vector_typed<STRSXP, uint64_t, rcpp_T::chr, NO_NULLS>(array);
 
         default:                                                      // # nocov
-            return Rcpp::LogicalVector(static_cast<R_xlen_t>(array,count_elements()), NA_LOGICAL); // # nocov
+            return Rcpp::LogicalVector(static_cast<R_xlen_t>(array.count_elements()), NA_LOGICAL); // # nocov
     }
 }
 
 
 template <int RTYPE>
 inline Rcpp::Vector<RTYPE> build_vector_mixed(simdjson::ondemand::array array) {
-    Rcpp::Vector<RTYPE> out(static_cast<R_xlen_t>(array,count_elements()));
+    Rcpp::Vector<RTYPE> out(static_cast<R_xlen_t>(array,.count_elements()));
     R_xlen_t            i(0L);
     for (auto element : array) {
         out[i++] = get_scalar_dispatch<RTYPE>(element);
@@ -92,7 +92,7 @@ inline Rcpp::Vector<RTYPE> build_vector_mixed(simdjson::ondemand::array array) {
 
 
 inline Rcpp::Vector<REALSXP> build_vector_integer64_mixed(simdjson::ondemand::array array) {
-    std::vector<int64_t> stl_vec_int64(static_cast<R_xlen_t>(array,count_elements()));
+    std::vector<int64_t> stl_vec_int64(static_cast<R_xlen_t>(array.count_elements()));
     std::size_t          i(0ULL);
 
     for (auto element : array) {
@@ -150,7 +150,7 @@ inline SEXP dispatch_mixed(simdjson::ondemand::array array, const rcpp_T common_
             return build_vector_mixed<LGLSXP>(array);
 
         default:
-            return Rcpp::LogicalVector(static_cast<R_xlen_t>(array,count_elements()), NA_LOGICAL);
+            return Rcpp::LogicalVector(static_cast<R_xlen_t>(array.count_elements()), NA_LOGICAL);
             // # nocov end
     }
 }
