7a6d2695da91417753cd3278174da9b7e13f9e13
Nicolas Boyer <nicolasjiaxin@gmail.com>
2021-08-26 20:33:42 +0000

    Update Type_Doctor.

--- inst/include/RcppSimdJson/deserialize/Type_Doctor.hpp
+++ inst/include/RcppSimdJson/deserialize/Type_Doctor.hpp
@@ -21,14 +21,21 @@ class Type_Doctor {
     bool STRING_ = false;
     bool chr_    = false;
 
-    bool NUMBER_ = false;
-    bool num_    = false;
+    bool DOUBLE_ = false;
+    bool dbl_    = false;
+
+    bool INT64_ = false;
+    bool i64_   = false;
+    bool i32_   = false;
 
     bool BOOL_ = false;
     bool lgl_  = false;
 
     bool NULL_VALUE_ = false;
     bool null_       = false;
+
+    bool UINT64_ = false;
+    bool u64_    = false;
 
 
   public:
@@ -70,10 +77,35 @@ inline Type_Doctor<type_policy, int64_opt>::Type_Doctor(simdjson::ondemand::arra
                 break;
 
             case simdjson::ondemand::json_type::number:
-                NUMBER_ = true;
-                num_    = true;
+                {
+                    simdjson::ondemand::number num = element.get_number();
+                    simdjson::ondemand::number_type t = num.get_number_type();
+                    switch (t) {
+                    case simdjson::ondemand::number_type::signed_integer:
+                        {
+                            INT64_ = true;
+                            if constexpr (int64_opt == utils::Int64_R_Type::Always) {
+                                i64_ = true;
+                            } else {
+                                if (utils::is_castable_int64(element.get_int64())) {
+                                    i32_ = true;
+                                } else {
+                                    i64_ = true;
+                                }
+                            }
+                        }
+                        break;
+                    case simdjson::ondemand::number_type::unsigned_integer:
+                        UINT64_ = true;
+                        u64_ = true;
+                        break;
+                    case simdjson::ondemand::number_type::floating_point_number:
+                        DOUBLE_ = true;
+                        dbl_ = true;
+                        break;
+                    }
+                }
                 break;
-
             case simdjson::ondemand::json_type::boolean:
                 BOOL_ = true;
                 lgl_  = true;
@@ -92,19 +124,25 @@ inline Type_Doctor<type_policy, int64_opt>::Type_Doctor(simdjson::ondemand::arra
 template <Type_Policy type_policy, utils::Int64_R_Type int64_opt>
 constexpr bool Type_Doctor<type_policy, int64_opt>::is_homogeneous() const noexcept {
     if (ARRAY_) {
-        return !(OBJECT_ || STRING_ || NUMBER_ || BOOL_); // # nocov
+        return !(OBJECT_ || STRING_ || DOUBLE_ || INT64_ || BOOL_ || UINT64_); // # nocov
     }
     if (OBJECT_) {
-        return !(STRING_ || NUMBER_ || BOOL_); // # nocov
+        return !(STRING_ || DOUBLE_ || INT64_ || BOOL_ || UINT64_); // # nocov
     }
     if (STRING_) {
-        return !(NUMBER_ || BOOL_);
+        return !(DOUBLE_ || INT64_ || BOOL_ || UINT64_);
     }
-    if (NUMBER_) {
-        return !(BOOL_);
+    if (DOUBLE_) {
+        return !(INT64_ || BOOL_ || UINT64_);
+    }
+    if (INT64_) {
+        return !(BOOL_ || UINT64_);
+    }
+    if (BOOL_) {
+        return !UINT64_;
     }
 
-    return BOOL_;
+    return UINT64_;
 }
 
 
@@ -119,30 +157,48 @@ inline constexpr rcpp_T Type_Doctor<type_policy, int64_opt>::common_R_type() con
 
     if constexpr (type_policy == Type_Policy::anything_goes) {
         return chr_ ? rcpp_T::chr
-                           : num_ ? rcpp_T::dbl
-                                        : lgl_ ? rcpp_T::lgl : rcpp_T::null;
+                    : u64_ ? rcpp_T::u64
+                           : dbl_ ? rcpp_T::dbl
+                                  : i64_ ? rcpp_T::i64
+                                         : i32_ ? rcpp_T::i32 : lgl_ ? rcpp_T::lgl : rcpp_T::null;
 
     } else {
-        if (chr_ && !(num_ || lgl_)) {
+        if (chr_ && !(dbl_ || i64_ || i32_ || lgl_ || u64_)) {
             return rcpp_T::chr;
         }
 
         if constexpr (type_policy == Type_Policy::strict) {
-            if (num_ && !(lgl_)) {
+            if (dbl_ && !(i64_ || i32_ || lgl_ || u64_)) {
                 return rcpp_T::dbl;
+            }
+            if (i64_ && !(i32_ || lgl_ || u64_)) {
+                return rcpp_T::i64;
+            }
+            if (i32_ && !(lgl_ || u64_)) {
+                return rcpp_T::i32;
             }
         }
 
         if constexpr (type_policy == Type_Policy::ints_as_dbls) {
-            if (num_ && !(lgl_)) { // any number will become double
+            if (dbl_ && !(lgl_ || u64_)) { // any number will become double
                 return rcpp_T::dbl;
+            }
+            if (i64_ && !(lgl_ || u64_)) {
+                // only 64/32-bit integers: will follow selected Int64_R_Type option
+                return rcpp_T::i64;
+            }
+            if (i32_ && !(lgl_ || u64_)) { // only 32-bit integers remaining: will become int
+                return rcpp_T::i32;
             }
         }
     }
 
 
-    if (lgl_) {
+    if (lgl_ && !u64_) {
         return rcpp_T::lgl;
+    }
+    if (u64_) {
+        return rcpp_T::u64;
     }
 
     return rcpp_T::null;
@@ -161,22 +217,32 @@ inline constexpr auto Type_Doctor<type_policy, int64_opt>::is_vectorizable() con
     }
 
     if (chr_) {
-        return !(num_ || lgl_);
+        return !(dbl_ || i64_ || i32_ || lgl_ || u64_);
     }
 
     if constexpr (type_policy == Type_Policy::strict) {
-        if (num_) {
-            return !(lgl_);
+        if (dbl_) {
+            return !(i64_ || i32_ || lgl_ || u64_);
+        }
+        if (i64_) {
+            return !(i32_ || lgl_ || u64_);
+        }
+        if (i32_) {
+            return !(lgl_ || u64_);
         }
     }
 
     if constexpr (type_policy == Type_Policy::ints_as_dbls) {
-        if (num_) {
-            return !(lgl_);
+        if (dbl_ || i64_ || i32_) {
+            return !(lgl_ || u64_);
         }
     }
 
-    return lgl_;
+    if (lgl_) {
+        return !u64_;
+    }
+
+    return u64_;
 }
 
 
@@ -186,12 +252,14 @@ inline constexpr simdjson::ondemand::json_type Type_Doctor<type_policy, int64_op
     using simdjson::ondemand::json_type;
 
     return ARRAY_
-               ?json_type::array
-               : OBJECT_ ?json_type::object
-                         : STRING_ ?json_type::string
-                                             : NUMBER_ ?json_type::number
-                                                                : BOOL_ ?json_type::boolean
-                                                                        :json_type::null;
+               ? json_type::array
+               : OBJECT_ ? json_type::object
+                         : STRING_ ? json_type::string
+                                   : UINT64_ ? json_type::number
+                                             : DOUBLE_ ? json_type::number
+                                                       : INT64_ ? json_type::number
+                                                                : BOOL_ ? json_type::boolean
+                                                                        : json_type::null;
 }
 
 
@@ -214,8 +282,34 @@ void Type_Doctor<type_policy, int64_opt>::add_element(simdjson::ondemand::value 
             break;
 
         case simdjson::ondemand::json_type::number:
-            NUMBER_ = true;
-            num_    = true;
+            {
+                simdjson::ondemand::number num = element.get_number();
+                simdjson::ondemand::number_type t = num.get_number_type();
+                switch (t) {
+                    case simdjson::ondemand::number_type::signed_integer:
+                        {
+                            INT64_ = true;
+                            if constexpr (int64_opt == utils::Int64_R_Type::Always) {
+                                i64_ = true;
+                            } else {
+                                if (utils::is_castable_int64(int64_t(element))) {
+                                    i32_ = true;
+                                } else {
+                                    i64_ = true;
+                                }
+                            }
+                        }
+                        break;
+                    case simdjson::ondemand::number_type::unsigned_integer:
+                        UINT64_ = true;
+                        u64_ = true;
+                        break;
+                    case simdjson::ondemand::number_type::floating_point_number:
+                        DOUBLE_ = true;
+                        dbl_ = true;
+                        break;
+                }
+            }
             break;
 
         case simdjson::ondemand::json_type::boolean:
@@ -243,14 +337,23 @@ inline constexpr void Type_Doctor<type_policy, int64_opt>::update(
     this->STRING_ = this->STRING_ || type_doctor2.STRING_;
     this->chr_    = this->chr_ || type_doctor2.chr_;
 
-    this->NUMBER_ = this->NUMBER_ || type_doctor2.NUMBER_;
-    this->num_    = this->num_ || type_doctor2.num_;
+    this->DOUBLE_ = this->DOUBLE_ || type_doctor2.DOUBLE_;
+    this->dbl_    = this->dbl_ || type_doctor2.dbl_;
+
+    this->INT64_ = this->INT64_ || type_doctor2.INT64_;
+    this->i64_   = this->i64_ || type_doctor2.i64_;
+    if constexpr (int64_opt != utils::Int64_R_Type::Always) {
+        this->i32_ = this->i32_ || type_doctor2.i32_;
+    }
 
     this->BOOL_ = this->BOOL_ || type_doctor2.BOOL_;
     this->lgl_  = this->lgl_ || type_doctor2.lgl_;
 
     this->NULL_VALUE_ = this->NULL_VALUE_ || type_doctor2.NULL_VALUE_;
     this->null_       = this->null_ || type_doctor2.null_;
+
+    this->UINT64_ = this->UINT64_ || type_doctor2.UINT64_;
+    this->u64_    = this->u64_ || type_doctor2.u64_;
 }
 
 
